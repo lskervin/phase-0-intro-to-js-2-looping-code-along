e14562a09098fadf274a33e8d2a74ef208701aab
LeightonSkervin <lskervin23@gmail.com>
2024-01-26 03:45:27 -0500

    Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(names, occasion) {
+    let cards = [];
+    for (let i = 0; i < names.length; i++) {
+      cards.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return cards;
+  }
+  
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  let names = ["Charlie", "Samip", "Ali"];
+  let occasion = "birthday";
+  let cards = writeCards(names, occasion);
+  console.log(cards);
+  countDown(10);
+  countDown(4)
